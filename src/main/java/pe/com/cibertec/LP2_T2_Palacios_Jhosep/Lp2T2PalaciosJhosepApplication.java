2b8b9b834ea7ba49f22a046c078241fd9948b4ec
Jhosep-Palacios-Allpocc <I202314569@CIBERTEC.EDU.PE>
2024-10-12 20:52:09 -0500

    Estructura inicial del T2

---
+++ src/main/java/pe/com/cibertec/LP2_T2_Palacios_Jhosep/Lp2T2PalaciosJhosepApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_Palacios_Jhosep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2PalaciosJhosepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2PalaciosJhosepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/LP2_T2_Palacios_Jhosep/Lp2T2PalaciosJhosepApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_Palacios_Jhosep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2PalaciosJhosepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
